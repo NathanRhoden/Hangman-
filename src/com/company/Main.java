179dc0bde6c62d93dbb8dd2b704b72644463fdca
Nathan <Nathan.rhoden@gmail.com>
2021-03-15 23:21:52 +0000

    added working Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,68 @@
 package com.company;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        int lifeCount = 8; // LIFECOUNTER WHEN IT FALLS TO ZERO GAME OVER
+
+
+
+        GameStart.welcomeMsg(); //SIMPLE GAME MSG
+        String randomWord  = GameBody.wordSelector();
+        char[] placeHolder = new char [randomWord.length()]; // CONTAINS "XXX" TO REVEAL LETTERS FROM WORD REVEAL
+
+        for (int i = 0 ; i < randomWord.length(); i++){
+
+            placeHolder[i] = 'X'; // POPULATES THE PLACEHOLDER WITH XXX'S TO BE REVEALED ON CORRECT GUESS
+        }
+
+
+
+
+
+        while(lifeCount > 0) {
+            char[] wordreveal  = randomWord.toCharArray(); // TURNS STRING INTO CHAR ARRAY FOR USE
+
+            char verify = GameBody.letterVerif(randomWord); // RETURNS LETTER
+
+
+
+            if(verify != 'x'){
+
+                int placeHolderLocal = new String(wordreveal).indexOf(verify);// GET THE LOCATION OF THE CORRECT LETTER
+
+                for(int k = 0 ; k < wordreveal.length; k++ )
+                {
+                    if(placeHolder[placeHolderLocal] == 'X'){
+                        placeHolder[placeHolderLocal] = verify;
+                        System.out.println(placeHolder);
+
+                    }
+                }
+            }
+            else {
+                lifeCount = lifeCount - 1 ;
+                System.out.println(lifeCount);
+
+            }
+
+
+
+        }
+
+
+
+
+
+
     }
+
+
+
 }
